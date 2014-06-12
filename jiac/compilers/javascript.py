07faf8db744bf76527c58877211600411064d669
Brian Morgan <brian.s.morgan@gmail.com>
2014-06-12 09:03:09 -0400

    add support for coffeescript and uglify-js

--- jiac/compilers/javascript.py
+++ jiac/compilers/javascript.py
@@ -1,13 +1,38 @@
-from rjsmin import jsmin
+import subprocess
 
 from . import CompilerMeta
 
 
 class JavaScriptCompiler(object):
     __metaclass__ = CompilerMeta
-    supported_mimetypes = ['text/javascript']
+    supported_mimetypes = ['text/javascript', 'text/coffeescript']
 
     @classmethod
     def compile(cls, what, mimetype='text/javascript', cwd=None,
                 uri_cwd=None, debug=None):
-        return jsmin(what)
+
+        output = what
+
+        if mimetype == 'text/coffeescript':
+            args = ['coffeescript', '-c', '-p', '-s']
+
+            handler = subprocess.Popen(args, stdout=subprocess.PIPE,
+                                       stdin=subprocess.PIPE,
+                                       stderr=subprocess.PIPE, cwd=None)
+
+            (stdout, stderr) = handler.communicate(input=output)
+            if handler.returncode == 0:
+                output = stdout
+            else:
+                raise RuntimeError('Test this :S %s' % stderr)
+
+        if debug:
+            args = ['uglify-js', '-', '-c']
+
+            (stdout, stderr) = handler.communicate(input=output)
+            if handler.returncode == 0:
+                output = stdout
+            else:
+                raise RuntimeError('Test this :S %s' % stderr)
+
+        return output
